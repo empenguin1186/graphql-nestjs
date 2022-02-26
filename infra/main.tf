9b8486b9e931f59873fcfaefb71309ced8d133ab
empenguin1186 <emperorpenguint1186@gmail.com>
2022-02-26 18:51:24 +0900

    :+1: add ci/cd

--- infra/main.tf
+++ infra/main.tf
@@ -12,15 +12,34 @@ provider "google" {
 }
 
 locals {
- backend_app_name  = "blog-training-backend-app"
- frontend_app_name = "blog-training-frontend-app"
+  backend_app_name  = "blog-training-backend-app"
+  frontend_app_name = "blog-training-frontend-app"
 }
 
 # Cloud Run のデプロイで利用するArtifact Registry のリポジトリ
 module "artifact-registry" {
- source                     = "./modules/artifact-registry"
- gcp_project_id             = var.gcp_project_id
- artifact_registry_location = var.primary_region
- backend_app_name           = local.backend_app_name
- frontend_app_name          = local.frontend_app_name
+  source                     = "./modules/artifact-registry"
+  gcp_project_id             = var.gcp_project_id
+  artifact_registry_location = var.primary_region
+  backend_app_name           = local.backend_app_name
+  frontend_app_name          = local.frontend_app_name
+}
+
+# Cloud SQL
+module "cloud-sql" {
+  source        = "./modules/cloud-sql"
+  target_region = var.primary_region
+}
+
+# Cloud Build
+# マイグレーション+バックエンドデプロイ
+# フロントエンドデプロイ
+module "cloud-build" {
+  source                      = "./modules/cloud-build"
+  gcp_project_id              = var.gcp_project_id
+  region                      = var.primary_region
+  cloudsql_instance_full_name = module.cloud-sql.blog_training_db_connection_name
+  backend_app_name            = local.backend_app_name
+  github_owner                = "empenguin1186"
+  github_app_repo_name        = "graphql-nestjs"
 }

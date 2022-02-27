0b9ddaf9de057bdf24c9bd624a9256807d837973
empenguin1186 <emperorpenguint1186@gmail.com>
2022-02-27 16:35:21 +0900

    :+1: IsNumber -> IsNuberString

--- backend/src/config/environments/env-validator.ts
+++ backend/src/config/environments/env-validator.ts
@@ -1,5 +1,5 @@
-import { IsEnum, IsNotEmpty, IsNumber, IsString, validateSync } from "class-validator";
 import { plainToClass } from "class-transformer";
+import { IsEnum, IsNotEmpty, IsNumberString, IsString, validateSync } from "class-validator";
 
 enum NodeEnvEnum {
   Development = 'development',
@@ -14,8 +14,8 @@ export class EnvValidator {
   @IsEnum(NodeEnvEnum)
   NODE_ENV: NodeEnvEnum;
 
-  @IsNumber()
-  PORT = 3333;
+  @IsNumberString()
+  PORT = 3000;
 
   @IsNotEmpty()
   @IsString()
